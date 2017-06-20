ac67fac5ba62a1cb0d5e12bd126defab9555c3db
raphael <moudens.raphael@orange.fr>
2017-06-20 16:58:18 +0200

    concact ok

--- public/app.js
+++ public/app.js
@@ -2,21 +2,11 @@
 
 console.log('merde ca colle avec tous');
 
-// // let name = prompt("Enter your name please");
-// // console.log(name);
-
-// // const hello = `*** Bonjour ${name} ! *** `;
-
-// // console.log(hello);
-
-// .addEventListener('click', e => { e.preventDefault(); });
-// let fruit = {name: 'banana', color: 'yellow'};
-// let describe = `This ${fruit.name} is ${fruit.color}`;
-// console.log(describe);
-
 document.getElementById('clickMe').addEventListener('click', function () {
-	document.getElementById('receiver').innerHTML = 'Bonjour '.concat(document.getElementById('content').value);
+	var Nom = { name: document.getElementById('content').value };
+	document.getElementById('receiver').innerHTML = 'bonjour ' + Nom.name;
 	if (document.getElementById('content').value == '') {
 		document.getElementById('receiver').innerHTML = 'Bonjour Pere Noël';
 	}
+	sessionStorage.setItem(('autosave', 'content').value);
 });
