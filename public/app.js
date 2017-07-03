8f7d93892f4178c844aab292026e59199a16cb41
raphael <moudens.raphael@orange.fr>
2017-07-03 11:55:33 +0200

    exo terminé

--- public/app.js
+++ public/app.js
@@ -1,12 +1,41 @@
 'use strict';
 
 console.log('merde ca colle avec tous');
+if (!localStorage.getItem('names')) {
+	var names = [];
+	localStorage.setItem('names', JSON.stringify(names));
+} else {
+	var _names = JSON.parse(localStorage.getItem('names'));
+	var hist = '';
+	_names.forEach(function (name) {
+		hist = hist + name + "<br/>";
+	});
+	document.getElementById('hist').innerHTML = hist;
+}
 
 document.getElementById('clickMe').addEventListener('click', function () {
-	var Nom = { name: document.getElementById('content').value };
-	document.getElementById('receiver').innerHTML = 'bonjour ' + Nom.name;
+	var nom = {
+		name: document.getElementById('content').value
+	};
+	var maj = nom.name.charAt(0).toUpperCase() + nom.name.substring(1).toLowerCase();
+	var names = JSON.parse(localStorage.getItem('names'));
+	names.push(maj);
+	var hist = '';
+	names.forEach(function (name) {
+		hist = hist + name + "<br/>";
+	});
+	document.getElementById('receiver').innerHTML = 'bonjour ' + maj;
+	document.getElementById('hist').innerHTML = hist;
 	if (document.getElementById('content').value == '') {
 		document.getElementById('receiver').innerHTML = 'Bonjour Pere Noël';
 	}
-	sessionStorage.setItem(('autosave', 'content').value);
+	localStorage.setItem('names', JSON.stringify(names));
+});
+document.getElementById('searchBtn').addEventListener('click', function () {
+	var names = JSON.parse(localStorage.getItem('names'));
+	var search = document.getElementById('search').value;
+	var filterHistory = names.filter(function (name) {
+		return name.toLowerCase().startsWith(search.toLowerCase());
+	});
+	document.getElementById('hist').innerHTML = filterHistory;
 });
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,11 +1,40 @@
 console.log('merde ca colle avec tous');
+if(!localStorage.getItem('names')){
+	let names = [];
+	localStorage.setItem('names',JSON.stringify(names));
+}else{
+	let names =JSON.parse(localStorage.getItem('names'));
+	let hist = '';
+	names.forEach(name => {
+		hist = hist + name +"<br/>";
+	})
+	document.getElementById('hist').innerHTML = hist;
+}
 
 document.getElementById('clickMe').addEventListener('click',() =>{
-	let Nom = {name: document.getElementById('content').value };
-	document.getElementById('receiver').innerHTML = `bonjour ${Nom.name}`;
+	let nom = {
+		name: document.getElementById('content').value 
+	};
+	let maj = nom.name.charAt(0).toUpperCase()+nom.name.substring(1).toLowerCase()
+	let names =JSON.parse(localStorage.getItem('names'));
+	names.push(maj);
+	let hist = '';
+	names.forEach(name => {
+		hist = hist + name +"<br/>";
+	})
+	document.getElementById('receiver').innerHTML = `bonjour ${maj}`;
+	document.getElementById('hist').innerHTML = hist;
 	if(document.getElementById('content').value == ''){
 		document.getElementById('receiver').innerHTML = 'Bonjour Pere Noël';
 	}
-	sessionStorage.setItem(('autosave','content').value);
+	localStorage.setItem('names',JSON.stringify(names));
 });
+document.getElementById('searchBtn').addEventListener('click', ()=>{
+	let names =JSON.parse(localStorage.getItem('names'));
+	let search = document.getElementById('search').value;
+	let filterHistory = names.filter(name => {
+		return name.toLowerCase().startsWith(search.toLowerCase());
 
+	});
+	document.getElementById('hist').innerHTML = filterHistory;
+})
